ab6cb6dfcefab5b0be98b4867c39b5be9542f32b
Yibo Gao <gaoyibo157@gmail.com>
2026-01-08 21:34:56 +0800

    Update env.sh for ubuntu and macos compatible setting

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -8,7 +8,8 @@ export WANDB_BASE_URL="http://124.222.155.174:8080"
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-# NVM bash completion disabled to avoid compinit conflict with Zimfw
-# Zimfw's completion module provides comprehensive completion support
-# [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+if [ -s "/opt/homebrew/opt/nvm/nvm.sh" ]; then
+  . "/opt/homebrew/opt/nvm/nvm.sh"
+elif [ -s "$NVM_DIR/nvm.sh" ]; then
+  . "$NVM_DIR/nvm.sh"
+fi
