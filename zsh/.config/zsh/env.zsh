55afcada1977f7b7387d2998942f9a0387ba5989
obiyoag <gaoyibo157@gmail.com>
2026-01-11 06:52:15 +0000

    update wandb url

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -4,7 +4,7 @@ export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="/Library/TeX/texbin:$PATH"
-export WANDB_BASE_URL="http://124.222.155.174:8080"
+export WANDB_BASE_URL="http://10.12.208.170:8080"
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 
 export NVM_DIR="$HOME/.nvm"
